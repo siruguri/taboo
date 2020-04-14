a9ea54974aec6cce3ad6c6b2e5adeb3a1a5d4f5e
Sameer Siruguri <siruguri@gmail.com>
2020-04-13 20:37:03 -0700

    made turns work

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,17 +3,21 @@ class Game < ApplicationRecord
   has_many :words, through: :word_selections
 
   has_many :game_players
-  has_many :players, class_name: 'User', through: :game_players
+  has_many :players, class_name: 'User', through: :game_players, source: :user
 
   has_one :turn
 
   def start!
-    turn.delete
+    turn&.delete
     u = players.sample
 
     order = players.select { |p| p.id != u.id }.map { |u| u.id }
     unless u.nil?
-      Turn.create game: self, player: u, order: order
+      Turn.create game: self, current_player: u, order: order
     end
+  end
+
+  def current_player
+    turn.current_player
   end
 end
